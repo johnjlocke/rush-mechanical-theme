daf411e35138dfe1f54e0ba5e66306e3150a97f5
John J. Locke <john@lockedowndesign.com>
2014-02-16 16:37:40 -0800

    Update home.php

--- rush-mechanical/home.php
+++ rush-mechanical/home.php
@@ -1,36 +1,42 @@
-<?php
-/*
- * Template Name: Home
- */
-
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-			  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			    <!-- <header class="entry-header">
-				<h1 class="entry-title"></h1>
-			     </header> -->
-
-	                     <div class="entry-content">
-		              <?php the_content(); ?>
-				<?php
-				   wp_link_pages( array(
-					'before' => '<div class="page-links">' . __( 'Pages:', 'rush-mechanical' ),
-					'after'  => '</div>',
-				    ) );
-				?>
-			     </div><!-- .entry-content -->
-			     <?php edit_post_link( __( 'Edit', 'rush-mechanical' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
-		          </article><!-- #post-## -->
-			<?php endwhile; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php require_once('home-fw.php'); ?>
+<?php
+/*
+ * Template Name: Home
+ */
+?>
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+			  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	                     <div class="entry-content">
+
+		              <?php the_content(); ?>
+
+				<?php
+				   wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'rush-mechanical' ),
+					'after'  => '</div>',
+				    ) );
+				?>
+
+			     </div><!-- .entry-content -->
+
+			     <?php edit_post_link( __( 'Edit', 'rush-mechanical' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+
+		          </article><!-- #post-## -->
+
+			<?php endwhile; ?>
+
+		</main><!-- #main -->
+
+	</div><!-- #primary -->
+	
+<?php get_sidebar(); ?>
+
+<?php require_once('home-fw.php'); ?>
 <?php get_footer(); ?>
